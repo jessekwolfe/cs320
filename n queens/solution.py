8dc29c013318761b287ebf840ac9c8fe03880667
Jesse Wolfe <C837071019@riyadh.cs.colostate.edu>
2025-09-12 13:20:00 -0600

    Add final solution
    add final script with tests. Formatted to pep8

--- n queens/solution.py
+++ n queens/solution.py
@@ -1,23 +1,66 @@
-def nQueensAll(int):
-    if(int < 4):
+# Solution to find all possible solutions of nqueens
+def nQueensAll(queen_count):
+    if queen_count < 4:
         raise ValueError("Input must be 4 or greater.")
+
+    def check_next_row(solution_so_far, total_queens):
+        current_index = len(solution_so_far)
+
+        if current_index == total_queens:
+            valid_solutions.append(translate_to_coordinates(solution_so_far))
+
+        for i in range(0, total_queens, 1):
+            unique_in_solution = i not in solution_so_far
+
+            if unique_in_solution and not_on_diagonal(solution_so_far, i):
+                check_next_row(solution_so_far + [i], total_queens)
+
     valid_solutions = []
-    y_cord = 1;
-    for i in range(0, int, 1):
-        valid_solutions.append((i, y_cord))
-        y_cord += 2
-        if(y_cord > int-1):
-            y_cord = 0
+
+    for i in range(0, queen_count, 1):
+        check_next_row([i], queen_count)
+
     return valid_solutions
 
+
+# Helper function for all nQueensAll solution
+def translate_to_coordinates(solution):
+    coordinate_solution = []
+
+    for index, number in enumerate(solution):
+        coordinate_solution.append((index, number))
+
+    return coordinate_solution
+
+
+# Helper function for all nQueensAll solution
+def not_on_diagonal(existing_solution, test_number):
+    # Because we are building as we go we do not need to check down only up.
+    test_index = len(existing_solution)
+
+    for index, number in enumerate(existing_solution):
+        diagonal_difference = test_index - index
+        if (test_number + diagonal_difference == number) or (
+            test_number - diagonal_difference == number
+        ):
+            return False
+
+    # If no diagonals hit
+    return True
+
+
+# To find just one solution in O(n).
 def nQueens(int):
-    if(int < 4):
+    if int < 4:
         raise ValueError("Input must be 4 or greater.")
+
     valid_solutions = []
-    y_cord = 1;
+    y_cord = 1
+
     for i in range(0, int, 1):
         valid_solutions.append((i, y_cord))
         y_cord += 2
-        if(y_cord > int-1):
+        if y_cord > int - 1:
             y_cord = 0
+
     return valid_solutions
--- n queens/test_solution.py
+++ n queens/test_solution.py
@@ -1,12 +1,13 @@
 import unittest
 from solution import nQueens, nQueensAll
 
+
 class TestNQueensFunction(unittest.TestCase):
     def test_4_nqueens(self):
         self.assertEqual(nQueens(4), [(0, 1), (1, 3), (2, 0), (3, 2)])
 
     def test_less_than_4_nqueens(self):
-        print("Running less than 4 queens test: ")
+        print("Running less than 4 queens test...")
         with self.assertRaises(ValueError) as err:
             nQueens(3)
         self.assertIn("Input must be 4 or greater.", str(err.exception))
@@ -18,7 +19,7 @@ class TestNQueensFunction(unittest.TestCase):
         self.assertIn("Input must be 4 or greater.", str(err.exception))
 
     def test_less_than_4_nQueensAll(self):
-        print("Running less than 4 queens test: ")
+        print("Running less than 4 queens test...")
         with self.assertRaises(ValueError) as err:
             nQueensAll(3)
         self.assertIn("Input must be 4 or greater.", str(err.exception))
@@ -29,8 +30,21 @@ class TestNQueensFunction(unittest.TestCase):
             nQueensAll(-4)
         self.assertIn("Input must be 4 or greater.", str(err.exception))
 
+    def test_4_nQueensAll(self):
+        self.assertEqual(
+            nQueensAll(4),
+            [[(0, 1), (1, 3), (2, 0), (3, 2)], [(0, 2), (1, 0), (2, 3), (3, 1)]],
+        )
+
+    def test_8_nQueensAll(self):
+        self.assertEqual(len(nQueensAll(8)), 92)
+
+    def test_10_nQueensAll(self):
+        self.assertEqual(len(nQueensAll(10)), 724)
+
     # def test_8_queens(self):
     #     self.assertEqual(add(0, 7), 7)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
