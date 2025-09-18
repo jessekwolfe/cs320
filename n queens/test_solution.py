2742a5c6d00b0585dadac3fb99b8fbcdf84711d5
Jesse Wolfe <C837071019@riyadh.cs.colostate.edu>
2025-09-18 14:13:12 -0600

    add back nqueens and add null test

--- n queens/test_solution.py
+++ n queens/test_solution.py
@@ -1,23 +1,50 @@
 import unittest
-from solution import nQueensAll
+from solution import nQueens, nQueensAll
+
 
 class TestNQueensFunction(unittest.TestCase):
+    def test_4_nqueens(self):
+        self.assertEqual(nQueens(4), [(0, 1), (1, 3), (2, 0), (3, 2)])
 
-    def test_n_queens_all(self):
-        print("Running nQueensAll test: ", end="")
-        self.assertTrue(nQueensAll())
-        if(self.assertTrue(nQueensAll()) == None):
-            print("Passed")
-            
-    # def test_4_queens(self):
-    #     self.assertEqual(solution.nQueensAll())
-    #     self.assertEqual(add(2, 3), 5)
+    def test_less_than_4_nqueens(self):
+        print("Running less than 4 queens test...")
+        with self.assertRaises(ValueError) as err:
+            nQueens(3)
+        self.assertIn("Input must be 4 or greater.", str(err.exception))
+        with self.assertRaises(ValueError) as err:
+            nQueens(0)
+        self.assertIn("Input must be 4 or greater.", str(err.exception))
+        with self.assertRaises(ValueError) as err:
+            nQueens(-4)
+        self.assertIn("Input must be 4 or greater.", str(err.exception))
 
-    # def test_less_than_4_queens(self):
-    #     self.assertEqual(add(-1, -5), -6)
+    def test_less_than_4_nQueensAll(self):
+        print("Running less than 4 queens test...")
+        with self.assertRaises(ValueError) as err:
+            nQueensAll(3)
+        self.assertIn("Input must be 4 or greater.", str(err.exception))
+        with self.assertRaises(ValueError) as err:
+            nQueensAll(0)
+        self.assertIn("Input must be 4 or greater.", str(err.exception))
+        with self.assertRaises(ValueError) as err:
+            nQueensAll(-4)
+        self.assertIn("Input must be 4 or greater.", str(err.exception))
+
+    def test_4_nQueensAll(self):
+        self.assertEqual(
+            nQueensAll(4),
+            [[(0, 1), (1, 3), (2, 0), (3, 2)], [(0, 2), (1, 0), (2, 3), (3, 1)]],
+        )
+
+    def test_8_nQueensAll(self):
+        self.assertEqual(len(nQueensAll(8)), 92)
+
+    def test_10_nQueensAll(self):
+        self.assertEqual(len(nQueensAll(10)), 724)
 
     # def test_8_queens(self):
     #     self.assertEqual(add(0, 7), 7)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
--- tree list/test_solution.py
+++ tree list/test_solution.py
@@ -3,8 +3,13 @@ from solution import findKey, deleteKey, addKey
 
 
 class TestTreeListFunction(unittest.TestCase):
-    def test_findKey(self):
-        pass
+    def test_findKey_non(self):
+        with self.assertRaises(ValueError) as err:
+            findKey(None, [1, 2, 3])
+        with self.assertRaises(ValueError) as err:
+            findKey(-1, [1, 2, 3])
+        with self.assertRaises(ValueError) as err:
+            findKey(3, [1, 2, 3])
 
 
 if __name__ == "__main__":
