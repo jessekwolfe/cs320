7904cf5c90e548e9d4519fbd2ce6185b35550edd
Jesse Wolfe <C837071019@riyadh.cs.colostate.edu>
2025-09-18 10:02:05 -0600

    Initialize lab 2

---
+++ tree list/solution.py
@@ -0,0 +1,8 @@
+def findKey(k, t):
+    pass
+
+def addKey(k, t):
+    pass
+
+def deleteKey(k, t):
+    pass
---
+++ tree list/test_solution.py
@@ -0,0 +1,11 @@
+import unittest
+from solution import findKey, deleteKey, addKey
+
+
+class TestTreeListFunction(unittest.TestCase):
+    def test_findKey(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
