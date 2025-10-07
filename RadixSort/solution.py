d6c5af3a235d6df0729fd9cecba3b71cffaee5aa
Jesse Wolfe <C837071019@riyadh.cs.colostate.edu>
2025-10-06 22:21:27 -0600

    add predefined exceptions

--- RadixSort/solution.py
+++ RadixSort/solution.py
@@ -1,2 +1,23 @@
 def radix_base(array, base):
+    try:
+        test_array(array)
+        test_base(base)
+    except ValueError:
+        raise
+
     return 1
+
+
+def test_array(array):
+    if array is None or not array:
+        raise ValueError("invalid␣arguments")
+    for element in array:
+        try:
+            element += 0
+        except:
+            raise ValueError("invalid␣list␣element")
+
+
+def test_base(base):
+    if not base or base < 2:
+        raise ValueError("invalid␣arguments")
--- RadixSort/test_solution.py
+++ RadixSort/test_solution.py
@@ -3,8 +3,28 @@ from solution import radix_base
 
 
 class TestNQueensFunction(unittest.TestCase):
-    def test_bases(self):
-        self.assertEqual(radix_base([], 2), 1)
+    def test_temp_bases(self):
+        self.assertEqual(radix_base([1, 2], 2), 1)
+
+    def test_empty_array_exception(self):
+        with self.assertRaises(ValueError) as err:
+            radix_base([], 2)
+
+    def test_none_array_exception(self):
+        with self.assertRaises(ValueError) as err:
+            radix_base(None, 2)
+
+    def test_base_too_low_exception(self):
+        with self.assertRaises(ValueError) as err:
+            radix_base([1, 2], 1)
+
+    def test_base_none_exception(self):
+        with self.assertRaises(ValueError) as err:
+            radix_base([1, 2], None)
+
+    def test_nonManthmatical_exception(self):
+        with self.assertRaises(ValueError) as err:
+            radix_base([1, "hello", 3], 2)
 
 
 if __name__ == "__main__":
