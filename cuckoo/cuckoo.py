9ea4b738516fc064a265dc164999e51bdbcdef3c
Jesse Wolfe <C837071019@riyadh.cs.colostate.edu>
2025-10-18 16:09:33 -0600

    Submittal 2

--- cuckoo/cuckoo.py
+++ cuckoo/cuckoo.py
@@ -2,9 +2,12 @@ from collections.abc import Collection
 from math import e, modf, floor, sqrt
 from itertools import filterfalse, chain
 from copy import copy
+import unittest
+
 
 # DO NOT CHANGE ANY CODE BETWEEN LINE X AND LINE Y
 # ******* THIS IS LINE X ******************
+
 GOLDEN = (1.0 + 5.0 * 0.5) / 2.0
 
 
@@ -15,7 +18,10 @@ def swap(a, b):
 # ***************************************************
 # why do we inherit from Collection rather than Set?
 # because Set requires too many methods to be defined
+
+
 class CuckooSet(Collection):
+
     # *** course helper routines *******
     def _hash2_(self, obj, table_size):
         try:
@@ -40,6 +46,7 @@ class CuckooSet(Collection):
         return chain(self._members_(self.htab1), self._members_(self.htab2))
 
     # ** course methods ****
+
     def __init__(self, iter=[], *, s=128):
         if s < 4:
             raise ValueError("set size too small")
@@ -61,7 +68,6 @@ class CuckooSet(Collection):
 
     def __str__(self):
         fstr = ""
-
         for v in self._allmembers_():
             if len(fstr):
                 fstr += ", "
@@ -73,14 +79,73 @@ class CuckooSet(Collection):
 
     # ******* THIS IS LINE Y ******************
 
-    def __contains__(x):
-        pass
+    def __contains__(self, x):
+        self.check_valid_value(x)
+        locations = self._hash2_(x, self._size_)
+        if self.htab1[locations[0]] == x or self.htab2[locations[1]] == x:
+            return True
+        return False
 
-    def add(x):
-        pass
+    def add(self, x):
+        self.check_valid_value(x)
+        if self.__contains__(x):
+            return
 
-    def remove(x):
-        pass
+        swapCount = 0
+        currentTable = self.htab1
+        currentObj = x
+        currentHash = self._hash2_(currentObj, self._size_)
 
-    def discard(x):
-        pass
+        if currentTable[currentHash[0]] is None:
+            currentTable[currentHash[0]] = x
+        else:
+            while swapCount < self._MAXSWAPS_:
+                currentObj = self.attemptToAddToTable(currentTable, currentObj)
+
+                if currentObj is None:  # Set to none if added without swap
+                    break
+                else:
+                    currentTable = self.swapCurrentTable(currentTable)
+                    swapCount += 1
+
+            if swapCount >= self._MAXSWAPS_:
+                self._resize_()
+                self.add(currentObj)
+
+    def swapCurrentTable(self, currTable):
+        if currTable is self.htab1:
+            return self.htab2
+        else:
+            return self.htab1
+
+    def attemptToAddToTable(self, currTable, val):
+        hash_index = 0
+        if currTable is not self.htab1:
+            hash_index = 1
+
+        hash_val = self._hash2_(val, self._size_)[hash_index]
+
+        if currTable[hash_val] is None:
+            currTable[hash_val] = val
+            return None
+        else:
+            temp = currTable[hash_val]
+            currTable[hash_val] = val
+            return temp
+
+    def remove(self, x):
+        if self.__contains__(x) is not True:
+            raise ValueError("key may not be None")
+        self.discard(x)
+
+    def discard(self, x):
+        self.check_valid_value(x)
+        locations = self._hash2_(x, self._size_)
+        if self.htab1[locations[0]] == x:
+            self.htab1[locations[0]] = None
+        if self.htab2[locations[1]] == x:
+            self.htab2[locations[1]] = None
+
+    def check_valid_value(self, x):
+        if x is None:
+            raise ValueError("key may not be None")
--- cuckoo/test_cuckoo.py
+++ cuckoo/test_cuckoo.py
@@ -4,9 +4,110 @@ from cuckoo import CuckooSet
 
 class Test_cuckoo(unittest.TestCase):
 
+    def setUp(self):
+        # small table to force collisions easily
+        self.s = 8
+        self.cs = CuckooSet(s=self.s)
+
     def test_cuckoo(self):
         s = CuckooSet()
         self.assertEqual(len(s), 0)
+
+    def test_add(self):
+        s = CuckooSet()
+        s.add(5)
+        self.assertEqual(len(s), 1)
+
+    def test_basic_insertion(self):
+        """Basic add() should store elements without error."""
+        self.cs.add("apple")
+        self.assertIn("apple", list(self.cs))
+        self.assertEqual(len(self.cs), 1)
+
+    def test_duplicate_insertion(self):
+        """Adding a duplicate should not increase set size."""
+        self.cs.add("apple")
+        self.cs.add("apple")
+        self.assertEqual(len(self.cs), 1)
+
+    def test_collision_and_swap(self):
+        """When collisions occur, cuckoo swapping should still succeed."""
+        # Artificially control hash collisions
+        # Use small table to trigger collisions quickly
+        data = ["A", "B", "C", "D"]
+        for item in data:
+            self.cs.add(item)
+        # All inserted items should be in the table
+        for item in data:
+            self.assertIn(item, list(self.cs))
+
+    def test_resize(self):
+        """If too many swaps happen, table should resize."""
+        old_size = self.cs._size_
+        # Force resize by filling table
+        self.cs._resize_()
+        self.assertGreater(self.cs._size_, old_size)
+
+    def test_max_swaps_trigger_resize(self):
+        """If too many swaps happen, table should resize."""
+        old_size = self.cs._size_ * 2
+        # Force resize by filling table
+        for i in range(old_size):
+            self.cs.add(f"item{i}")
+        # Add another to exceed MAXSWAPS and trigger resize
+        self.cs.add("overflow")
+        self.assertGreater(self.cs._size_, old_size)
+
+    def test_all_items_accessible_after_resize(self):
+        """After resize, all inserted items remain accessible."""
+        items = [f"item{i}" for i in range(50)]
+        for i in items:
+            self.cs.add(i)
+        for i in items:
+            self.assertIn(i, list(self.cs))
+
+    def test_str_output(self):
+        """__str__ should include inserted items."""
+        self.cs.add("apple")
+        self.cs.add("banana")
+        s = str(self.cs)
+        self.assertIn("apple", s)
+        self.assertIn("banana", s)
+
+    def test_iteration(self):
+        """__iter__ should yield all members."""
+        items = {"x", "y", "z"}
+        for i in items:
+            self.cs.add(i)
+        self.assertEqual(set(self.cs), items)
+
+    def test_contains(self):
+        old_size = self.cs._size_
+        # Force resize by filling table
+        for i in range(old_size):
+            self.cs.add(f"item{i}")
+        # Add another to exceed MAXSWAPS and trigger resize
+        self.assertTrue(self.cs.__contains__("item1"))
+
+    def test_remove(self):
+        old_size = self.cs._size_
+        # Force resize by filling table
+        for i in range(old_size):
+            self.cs.add(f"item{i}")
+        # Add another to exceed MAXSWAPS and trigger resize
+        self.assertTrue(self.cs.__contains__("item1"))
+        self.cs.remove("item1")
+        self.assertFalse(self.cs.__contains__("item1"))
+
+    def test_remove_exception(self):
+        old_size = self.cs._size_
+        # Force resize by filling table
+        for i in range(old_size):
+            self.cs.add(f"item{i}")
+        # Add another to exceed MAXSWAPS and trigger resize
+        with self.assertRaises(ValueError) as context:
+            self.cs.remove("Non-Existant")
+        self.assertEqual(str(context.exception), "key may not be None")
 
 
 if __name__ == "__main__":
