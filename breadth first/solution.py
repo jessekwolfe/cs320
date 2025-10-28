3b37f4c96acd6c667c65f50f0cbb0139995dbe61
Jesse Wolfe <C837071019@riyadh.cs.colostate.edu>
2025-10-27 19:02:42 -0600

    BFS log 0

--- breadth first/solution.py
+++ breadth first/solution.py
@@ -1,5 +1,37 @@
 from edgegraph import GraphEL, VertexEL
 
 
-def bfs(graph: GraphEL, start: VertexEL) -> list:
-    pass
+def bfs(graph, start) -> list:
+    try:
+        validParams(graph, start)
+    except ValueError:
+        raise
+
+    lot = []
+    try:
+        lot.append(graph.find_vertex(start))
+    except KeyError:
+        return lot
+
+    return lot
+
+
+def validParams(graph, start):
+    if graph is None or start is None:
+        raise ValueError("Invalid graph or vertex")
+
+
+# Algorithm BFS(G,s)
+#     Input:    An undirected graph and a vertex of Output: A labeling of edges in the connected compontent of as tree edges and cross edges
+
+#     Create an empty list, L0
+#     Mark as explored and insert s into L0
+#     while Li is not empty do
+#         Create an empty list, Li+1
+#         for each vertex, u, in Li do
+#             for each edge, e=(u,v), incident to v in G do
+#                 if vertex v is unexplored then
+#                     Label {e} as a tree edge for discovering vertex v
+#                     Mark as explored and insert {v} into {L(i+1)}
+#                 else
+#                     Label {e} as a cross or back edge
--- breadth first/test_bfs.py
+++ breadth first/test_bfs.py
@@ -4,11 +4,29 @@ from solution import bfs
 
 
 class Test_bfs(unittest.TestCase):
-    def test_bfs(self):
+    def test_None_params(self):
+        ve = VertexEL("test")
+        ge = GraphEL()
+        with self.assertRaises(ValueError) as context:
+            bfs(None, ve)
+        self.assertEqual(str(context.exception), "Invalid graph or vertex")
+        with self.assertRaises(ValueError) as context:
+            d = bfs(ge, None)
+        self.assertEqual(str(context.exception), "Invalid graph or vertex")
+
+    def test_Start_Not_in_Graph(self):
         ve = VertexEL("test")
         ge = GraphEL()
         d = bfs(ge, ve)
-        self.assertIsNone(d)
+        self.assertEqual(d, [])
+
+    def test_Single_Vertex_graph(self):
+        ve = VertexEL("test")
+        ge = GraphEL()
+        ge.add_vertex(ve)
+        d = bfs(ge, ve)
+        self.assertTrue(len(d) == 1)
+        self.assertEqual(d, [ve])
 
 
 if __name__ == "__main__":
